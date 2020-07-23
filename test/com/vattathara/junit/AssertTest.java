de7f20b6a9f7f2ac46f527c8f8c2c188eccaa37c
Robinclear <robin.peter.vkm@gmail.com>
2020-07-23 14:17:21 -0300

    AssertEquals basics

---
+++ test/com/vattathara/junit/AssertTest.java
@@ -0,0 +1,14 @@
+package com.vattathara.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AssertTest {
+
+	@Test
+	void test() {
+		assertEquals(1, 1);
+	}
+
+}
